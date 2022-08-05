cf47a2299bba8430a75e35c2436da7b9c12abbb6
hsh <hsh9588@naver.com>
2022-08-05 15:13:16 +0900

    00_javaTest 프로젝트 추가
    클래스 작성법

---
+++ 00_javaTest/src/edu/kh/test/JavaTest.java
@@ -0,0 +1,20 @@
+package edu.kh.test;
+
+//한 줄 주석: 컴파일러(번역기)가 해석하지 않는부분이다
+/*
+ 범위 주석:
+ */
+
+// class: 자바 코드가 작성되는 영역이다.
+public class JavaTest {
+
+	//main 메서드임. 이 public static 어쩌구{}여기까지를 메인메서드라고 부름.
+	//자바 애플리케이션(=프로그램)을 실행하기 위해서 반드시 필요한 구문
+	public static void main(String[] args) {
+		
+		System.out.println("안녕하세요");
+		//System.out.println("println()은 한 줄 출력입니다.");
+		System.out.println(6+200+300 + 400);
+	}
+	
+}
