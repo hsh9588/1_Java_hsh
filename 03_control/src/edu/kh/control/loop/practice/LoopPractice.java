fb84e8b532082c242eba8b797bb1fa6565f78da3
hsh9588 <hsh9588@naver.com>
2022-08-11 10:51:00 +0900

    20220811 실습문제 풀이중

--- 03_control/src/edu/kh/control/loop/practice/LoopPractice.java
+++ 03_control/src/edu/kh/control/loop/practice/LoopPractice.java
@@ -89,6 +89,45 @@ public class LoopPractice {
 			System.out.println("1이상의 숫자를 입력해주세요.");
 		}
 
+		// 풀이
+		// 시작에 앞서 헷갈릴 때에는 안되는 것부터 시작해보자.
+
+		/*
+		 * 방법 1 : if (input1 < 1 || input2 < 1) { // num1, num2 중에 0이 있는가?
+		 * System.out.println("1 이상의 숫자를 입력해주세요/");
+		 * 
+		 * } else { // 둘 다 0이 아니다
+		 * 
+		 * // input1 < input2 for (int i = input1 ; i <= input2 ; i++) {
+		 * System.out.print(i+ " "); } // input1 > input2 for (int i = input2 ; i <=
+		 * input1 ; i++) { System.out.print(i+ " "); } } 방법 2 : if (input1 < 1 || input2
+		 * < 1) { // num1, num2 중에 0이 있는가? System.out.println("1 이상의 숫자를 입력해주세요");
+		 * 
+		 * } else { // 둘 다 0이 아니다 int start = input1; int end = input2;
+		 * 
+		 * if (input1 > input2) { start = input2; end = input1; } for(int i = start ; i
+		 * <= end ; i++) { System.out.print(i + " "); }
+		 * 
+		 * }
+		 * 
+		 * 
+		 * 방법 3 : if (input1 < 1 || input2 < 1) { // num1, num2 중에 0이 있는가?
+		 * System.out.println("1 이상의 숫자를 입력해주세요");
+		 * 
+		 * } else { // 둘 다 0이 아니다
+		 * 
+		 * if ( input1 > input2) {
+		 * 
+		 * int temp = input1;
+		 * 
+		 * input1 = input2;
+		 * 
+		 * input2 = temp; }
+		 * 
+		 * for (int i = input1 ; i <= input2 ; i++) { System.out.print(i + " "); } }
+		 * 
+		 */
+
 	}
 
 	public void practice5() {
@@ -124,6 +163,15 @@ public class LoopPractice {
 			}
 		}
 
+		// 풀이
+		/*
+		 * if (dan >= 2 && dan <= 9) {
+		 * 
+		 * for (int i = dan ; i <= 9 ; i++) { System.out.println("===== " + i +
+		 * "단 ====="); for (int x = 1 ; x <= 9 ; x ++) { System.out.println(x + " * " +
+		 * i + " = " + x * i); } System.out.println(); // 단 사이 줄 바꿈 } } else {
+		 * System.out.println("2 ~ 9 사이의 숫자만 입력해주세요."); }
+		 */
 	}
 
 	public void practice7() {
@@ -165,36 +213,131 @@ public class LoopPractice {
 		System.out.print("정수 입력 : ");
 		int star = sc.nextInt();
 
-		for (int row = star; row >= 1; row--) {
-			for (int col = 1; col <= star; col++) {
-				if (row <= col) {
-					System.out.print("*");
-				} else {
-					System.out.print(" ");
-				}
+//		for (int row = star; row >= 1; row--) {
+//			for (int col = 1; col <= star; col++) {
+//				if (row <= col) {
+//					System.out.print("*");
+//				} else {
+//					System.out.print(" ");
+//				}
+//			}
+//			System.out.println();
+//		}
+
+		for (int row = 1; row <= star; row++) {
+			for (int col = 1; col <= star - row; col++) {
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= row; k++) {
+				System.out.print("*");
 			}
 			System.out.println();
 		}
 
+		// 풀이
+		/*
+		 * 
+		 * // ex. // 정수 입력 : 4 // * // ** // *** // ****
+		 * 
+		 * // for 문을 이용한 풀이
+		 * 
+		 * for (int row = 1; row <= star ; row++) {
+		 * 
+		 * for (int i = star-1 ; i >= 1 ; i--) {
+		 * 
+		 * // for (int i = star-row ; i >= 1 ; i--) { // star == 4
+		 * 
+		 * System.out.print(" ");
+		 * 
+		 * }
+		 * 
+		 * for (int col = 1 ; col <= row ; col++) { System.out.print("*"); }
+		 * System.out.println(); // 줄 바꿈 }
+		 * 
+		 * // if 문을 이용한 풀이
+		 * 
+		 * for (int row = 1; row <= star ; row++) { for (int col = 1; col <= star ;
+		 * col++) {
+		 * 
+		 * if (col <= star - row) { System.out.print(" ");
+		 * 
+		 * } else { System.out.print("*");
+		 * 
+		 * } } System.out.println(); // 줄 바꿈 }
+		 * 
+		 * 
+		 */
 	}
 
 	public void practice10() {
 
 		Scanner sc = new Scanner(System.in);
 
+//		System.out.print("정수 입력 : ");
+//		int star = sc.nextInt();
+//
+//		for (int row = 1; row <= star; row++) {
+//			for (int col = 1; col <= row; col++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//
+//		for (int row = 1; row <= star; row++) {
+//			for (int col = row; col <= star - 1; col++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+
+		// 풀이
+
+//		정수 입력 : 3
+
+//		*
+//		**
+//		***
+//		**
+//		*
+
 		System.out.print("정수 입력 : ");
-		int star = sc.nextInt();
+		int input = sc.nextInt();
+//
+//		// 위쪽 삼각형
+//
+//		for (int row = 1; row <= input; row++) {
+//
+//			for (int col = 1; col <= row; col++) {
+//
+//				System.out.print("*");
+//			}
+//			System.out.println(); // 줄 바꿈
+//		}
+//		// 아랫쪽 삼각형
+//
+//		for (int row = input - 1 ; row >= 1; row--) {
+//
+//			for (int col = 1; col <= row; col++) {
+//
+//				System.out.print("*");
+//			}
+//			System.out.println(); // 줄 바꿈
+//		}
 
-		for (int row = 1; row <= star; row++) {
-			for (int col = 1; col <= row; col++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
+		System.out.println("==========================");
 
-		for (int row = 1; row <= star; row++) {
-			for (int col = row; col <= star - 1; col++) {
-				System.out.print("*");
+		// 풀이 아직 미완성
+		for (int row = 1; row <= input * 2 - 1; row++) {
+
+			if (row < input) {
+
+				for (int col = 1; col < row; col++) {
+					System.out.print("*");
+				}
+			} else {
+				for (int col = input; col > row - input; col--) {
+					System.out.print("*");
+				}
 			}
 			System.out.println();
 		}
@@ -218,17 +361,46 @@ public class LoopPractice {
 //			}
 //			System.out.println();
 //		}		
-	
+
 		for (int row = 1; row <= star; row++) {
-			for (int col = 1; col <= star - row; col++) {	
+			for (int col = 1; col <= star - row; col++) {
 				System.out.print(" ");
 			}
-			for (int i = 1 ; i <= 2 * row - 1; i++) {
+			for (int i = 1; i <= 2 * row - 1; i++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
 
+//		System.out.print("정수 입력 : ");
+//		int input = sc.nextInt();
+//
+//		// ex. // 정수 입력 : 4 // * // ** // *** // ****
+//
+//		// for 문을 이용한 풀이 아직 미완성
+//
+//		for (int row = 1; row <= input; row++) {
+//
+//			for (int i = input - 1; i >= 1; i--) {
+//		// for (int i = star-row ; i >= 1 ; i--) { // star == 4
+//				System.out.print(" ");
+//			}
+//
+//			for (int col = 1; col <= row; col++) {
+//				System.out.print("*");
+//			}
+//			System.out.println(); // 줄 바꿈
+//		}
+		
+		// if문을 이용한 풀이 미완성
+		
+//		for (int col = 1 ; col <= input ; col++) {
+//			if (input - row >= col || input + row <= col) {
+//				System.out.print(" ");
+//			} else {
+//				System.out.print("*");
+//			}
+//		}
 	}
 
 	public void practice12() {
@@ -240,7 +412,7 @@ public class LoopPractice {
 
 		for (int row = star; row >= 1; row--) {
 			for (int col = 1; col <= star; col++) {
-				if (col <= 1 || col >= star || row <= 1 || row >= star) {
+				if (col == 1 || col == star || row == 1 || row == star) {
 					System.out.print("*");
 				} else {
 					System.out.print(" ");
@@ -248,6 +420,17 @@ public class LoopPractice {
 			}
 			System.out.println();
 		}
+		
+		// 풀이 미완성
+		System.out.print("정수 입력 : ");
+		int input = sc.nextInt();
+		
+		for (int row = 1 ; row <= input ; row++) {
+			for (int col = 1 ; col <= input ; col++) {
+				
+			}
+		}
+		
 
 	}
 
@@ -258,14 +441,16 @@ public class LoopPractice {
 		System.out.print("자연수 하나를 입력하세요 : ");
 		int input = sc.nextInt();
 
-		int sum = 0;
 		int count = 0;
 
+		// 2의 배수 또는 3의 배수인 경우 출력
 		for (int i = 1; i <= input; i++) {
 			if (i % 2 == 0 || i % 3 == 0) {
 				System.out.print(i + " ");
-				if (i % 6 == 0) {
-					count++;
+				
+				// 2와 3의 공배수 == 2로도 나누어 떨어지고. 3으로도 나누어 떨어진다.
+				if (i % 6 == 0) { 
+					count++; // count 증가
 				}
 			}
 		}
