5764aa76e505c28c38b607c041d813af81b46dc5
hsh9588 <hsh9588@naver.com>
2022-08-16 17:51:04 +0900

    20220816 수업종료

--- 00_javaTest/src/edu/kh/test/Test.java
+++ 00_javaTest/src/edu/kh/test/Test.java
@@ -1,8 +1,18 @@
 package edu.kh.test;
 
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
-	}
 
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("숫자 : ");
+		int input = sc.nextInt();
+		
+		for (int row = 1; row <= input; row++) {
+			for (int col = 1; col <= input; )
+		}
+	}
 }
